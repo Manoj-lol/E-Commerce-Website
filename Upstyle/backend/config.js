5dc66465e1ffbae909f3e7cd75d8fe2b5d37ec22
Sai Manoj <62412938+Manoj-lol@users.noreply.github.com>
2020-12-08 15:49:42 +0530

    Add files via upload

---
+++ Upstyle/backend/config.js
@@ -0,0 +1,4 @@
+export default {
+    MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/Upstyle',
+    JWT_SECRET: process.env.JWT_SECRET || 'something secret'
+}
---
+++ Upstyle/backend/data.js
@@ -0,0 +1,37 @@
+export default {
+    products:[
+        {
+            _id: '1',
+            name: 'Slim Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 1999,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6
+        },
+        {
+            _id: '2',
+            name: 'Fit Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 1799,
+            brand: 'Nike',
+            rating: 4.3,
+            numReviews: 24,
+            countInStock: 8
+        },
+        {
+            _id: '3',
+            name: 'Best Pants',
+            category: 'Pants',
+            image: '/images/d1.jpg',
+            price: 1599,
+            brand: 'Nike',
+            rating: 4.7,
+            numReviews: 12,
+            countInStock: 10
+        }
+    ]
+}
